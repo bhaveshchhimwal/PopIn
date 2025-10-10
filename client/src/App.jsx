4c805ac305030c2bc558bd59f4b907127ca443cb
bhaveshchhimwal <socialnetworkbhavesh@gmail.com>
2025-10-10 15:12:34 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-// src/App.jsx
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 import AuthLayoutBuyer from "./components/AuthLayoutBuyer";
@@ -8,14 +7,9 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Landing */}
         <Route path="/" element={<LandingPage />} />
-
-        {/* Buyer auth */}
         <Route path="/auth/login" element={<AuthLayoutBuyer defaultMode="login" />} />
         <Route path="/auth/register" element={<AuthLayoutBuyer defaultMode="register" />} />
-
-        {/* Seller auth */}
         <Route path="/seller/login" element={<AuthLayoutSeller defaultMode="login" />} />
         <Route path="/seller/register" element={<AuthLayoutSeller defaultMode="register" />} />
       </Routes>
