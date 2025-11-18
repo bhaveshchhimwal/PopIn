9b98a457c7a6f5ea527505b7f45cd37d7c89bfe3
bhaveshchhimwal <socialnetworkbhavesh@gmail.com>
2025-11-18 22:32:50 +0530

    fix url

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+const isLocalhost = window.location.hostname === 'localhost' || 
+                    window.location.hostname === '127.0.0.1';
+
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8080/api', // Backend URL
+  baseURL: isLocalhost 
+    ? 'http://localhost:8080/api' 
+    : 'https://popin-evz9.onrender.com/api',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
